fff867339dc5370792451c33cb511925d6489e79
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2023-10-07 22:20:12 +0200

    added mass-spring system

---
+++ mass_spring/mass_spring.cc
@@ -0,0 +1,31 @@
+#include "mass_spring.h"
+
+
+int main()
+{
+  MassSpringSystem<2> mss;
+  mss.SetGravity( {0,-9.81} );
+  auto fA = mss.AddFix( { 0.0, 0.0 } );
+  auto mA = mss.AddMass( 1, { 1.0, 0.0 } );
+  mss.AddSpring (1, 10, fA, mA );
+
+  auto mB = mss.AddMass( 1, { 2.0, 0.0 } );
+  mss.AddSpring (1, 20, mA, mB);
+  
+  cout << "mss: " << endl << mss << endl;
+
+  auto mss_func = make_shared<MSS_Function<2>> (mss);
+
+
+
+  double tend = 10;
+  double dt = tend/1000;
+  
+  Vector<> x(2*mss.Masses().size());
+  for (size_t i = 0; i < mss.Masses().size(); i++)
+    x.Range(2*i,2*i+2) = mss.Masses()[i].pos;
+  
+  SolveODE_Verlet(tend, dt, x, mss_func,
+                  [](double t, VectorView<double> x) { cout << "t = " << t
+                                                            << ", x = " << Vec<4>(x) << endl; });
+}
---
+++ mass_spring/mass_spring.h
@@ -0,0 +1,164 @@
+#ifndef MASS_SPRING_H
+#define MASS_SPRING_H
+
+
+
+#include <nonlinfunc.h>
+#include <ode.h>
+
+using namespace ASC_ode;
+
+#include <vector.hpp>
+using namespace ngbla;
+
+
+
+template <int D>
+class Mass
+{
+public:
+  double mass;
+  Vec<D> pos;
+};
+
+class Connection
+{
+public:
+  enum CONTYPE { FIX=1, MASS=2 };
+  CONTYPE type;
+  size_t nr;
+};
+
+ostream & operator<< (ostream & ost, const Connection & con)
+{
+  ost << "type = " << int(con.type) << ", nr = " << con.nr;
+  return ost;
+}
+
+class Spring
+{
+public:
+  double length;  
+  double stiffness;
+  Connection connections[2];
+};
+
+template <int D>
+class MassSpringSystem
+{
+  std::vector<Vec<D>> fixes;
+  std::vector<Mass<D>> masses;
+  std::vector<Spring> springs;
+  Vec<D> gravity=0.0;
+public:
+  void SetGravity (Vec<D> _gravity) { gravity = _gravity; }
+  Vec<D> Gravity() const { return gravity; }
+  
+  Connection AddFix (Vec<D> p)
+  {
+    fixes.push_back(p);
+    return { Connection::FIX, fixes.size()-1 };
+  }
+
+  Connection AddMass (double mass, Vec<D> p)
+  {
+    masses.push_back (Mass<D>{mass, p});
+    return { Connection::MASS, masses.size()-1 };
+  }
+
+  size_t AddSpring (double length, double stiffness, Connection c1, Connection c2)
+  {
+    springs.push_back (Spring{length, stiffness, { c1, c2 } });
+    return springs.size()-1;
+  }
+
+  auto & Fixes() const { return fixes; } 
+  auto & Masses() const { return masses; } 
+  auto & Springs() const { return springs; } 
+};
+
+template <int D>
+ostream & operator<< (ostream & ost, const MassSpringSystem<D> mss)
+{
+  ost << "fixes:" << endl;
+  for (auto f : mss.Fixes())
+    ost << f << endl;
+
+  ost << "masses: " << endl;
+  for (auto m : mss.Masses())
+    ost << "m = " << m.mass << ", pos = " << m.pos << endl;
+
+  ost << "springs: " << endl;
+  for (auto sp : mss.Springs())
+    ost << "length = " << sp.length << "stiffness = " << sp.stiffness
+        << ", C1 = " << sp.connections[0] << ", C2 = " << sp.connections[1] << endl;
+  return ost;
+}
+
+
+template <int D>
+class MSS_Function : public NonlinearFunction
+{
+  const MassSpringSystem<D> & mss;
+public:
+  MSS_Function (const MassSpringSystem<D> & _mss)
+    : mss(_mss) { }
+
+  virtual size_t DimX() const { return D*mss.Masses().size(); }
+  virtual size_t DimF() const { return D*mss.Masses().size(); }
+  
+  virtual void Evaluate (VectorView<double> x, VectorView<double> f) const
+  {
+    f = 0.0;
+    
+    auto xmat = x.AsMatrix(mss.Masses().size(), 2);
+    auto fmat = f.AsMatrix(mss.Masses().size(), 2);
+    
+    for (size_t i = 0; i < mss.Masses().size(); i++)
+      fmat.Row(i) = mss.Masses()[i].mass*mss.Gravity();
+    
+    for (auto spring : mss.Springs())
+      {
+        auto [c1,c2] = spring.connections;
+        Vec<D> p1, p2;
+        if (c1.type == Connection::FIX)
+          p1 = mss.Fixes()[c1.nr];
+        else
+          p1 = xmat.Row(c1.nr);
+        if (c2.type == Connection::FIX)
+          p2 = mss.Fixes()[c2.nr];
+        else
+          p2 = xmat.Row(c2.nr);
+
+        double force = spring.stiffness * (L2Norm(p1-p2)-spring.length);
+        Vec<D> dir12 = 1.0/L2Norm(p1-p2) * (p2-p1);
+        if (c1.type == Connection::MASS)
+          fmat.Row(c1.nr) += force*dir12;
+        if (c2.type == Connection::MASS)
+          fmat.Row(c2.nr) -= force*dir12;
+      }
+
+    for (size_t i = 0; i < mss.Masses().size(); i++)
+      fmat.Row(i) /= mss.Masses()[i].mass;
+  }
+  
+  virtual void EvaluateDeriv (VectorView<double> x, MatrixView<double> df) const
+  {
+    // TODO: exact differentiation
+    double eps = 1e-6;
+    Vector<> xl(DimX()), xr(DimX()), fl(DimF()), fr(DimF());
+    for (size_t i = 0; i < DimX(); i++)
+      {
+        xl = x;
+        xl(i) -= eps;
+        xr = x;
+        xr(i) += eps;
+        Evaluate (xl, fl);
+        Evaluate (xr, fr);
+        df.Col(i) = 1/(2*eps) * (fr-fl);
+      }
+  }
+  
+};
+
+#endif
